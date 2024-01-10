345349c6a7cd273fb519f747f301ca63ee156182
Uriel Saldana <uriel.saldana@REM-MC-231030A.local>
2024-01-10 16:59:01 -0600

    first commit

---
+++ config/ci.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisUrl: 'redis://127.0.0.1:6379',
+    googleClientID:
+        '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+    googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+        //'mongodb+srv://urielsaldanadev:DCUiY193kvB0oZay@cluster0.54u2nht.mongodb.net/blog_dev?retryWrites=true&w=majority',
+    cookieKey: '123123123',
+};
---
+++ config/dev.js
@@ -0,0 +1,19 @@
+/*
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI:
+    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+};
+*/
+module.exports = {
+    redisUrl: 'redis://127.0.0.1:6379',
+    googleClientID:
+        '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+    googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    mongoURI:
+        'mongodb+srv://urielsaldanadev:DCUiY193kvB0oZay@cluster0.54u2nht.mongodb.net/blog_dev?retryWrites=true&w=majority',
+    cookieKey: '123123123',
+};
